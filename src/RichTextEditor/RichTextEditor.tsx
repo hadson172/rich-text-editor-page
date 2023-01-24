6aae8ef9b2c08caa8bfb664d9b0fa802fab94ebe
phalas <piothalas@gmail.com>
2023-01-24 14:03:45 +0100

    Syled editor, added debugger

--- src/RichTextEditor/RichTextEditor.tsx
+++ src/RichTextEditor/RichTextEditor.tsx
@@ -1,13 +1,28 @@
+import { ReactNode, useRef, useState } from "react";
 import RichTextEditorContent from "./components/EditorContent/EditorContent";
 import RichTextEditorHeader from "./components/EditorHeader/EditorHeader";
 
 import "./RichTextEditor.css";
 
 const RichTextEditor = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [debuggerState, setDebuggerState] = useState<string | null>(null);
+
   return (
-    <div className="editor">
-      <RichTextEditorHeader />
-      <RichTextEditorContent />
+    <div className="editor__wrapper">
+      <div className="editor">
+        <RichTextEditorHeader />
+        <RichTextEditorContent ref={ref} />
+      </div>
+      <button
+        onClick={() => setDebuggerState(ref.current?.innerHTML ?? null)}
+      >
+        Show
+      </button>
+      <div className="editor__debugger">
+        <h2>Debugger:</h2>
+        <div className="debugger__content">{debuggerState}</div>
+      </div>
     </div>
   );
 };
--- src/RichTextEditor/components/EditorContent/EditorContent.tsx
+++ src/RichTextEditor/components/EditorContent/EditorContent.tsx
@@ -1,13 +1,20 @@
+import { forwardRef, ReactNode, useEffect, useRef } from "react";
 import "./EditorContent.css";
 
-const RichTextEditorContent = () => {
-  return (
-    <div
-      suppressContentEditableWarning={true}
-      contentEditable
-      className="editor__content"
-    ></div>
-  );
-};
+type Props = {};
+
+const RichTextEditorContent = forwardRef<HTMLDivElement, Props>(
+  (props, ref) => {
+    return (
+      <div
+        ref={ref}
+        suppressContentEditableWarning={true}
+        contentEditable
+        className="editor__content"
+        onChange={(value) => console.log({ value })}
+      ></div>
+    );
+  }
+);
 
 export default RichTextEditorContent;
